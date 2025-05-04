0ab8d4d9cfea483a0a101709aaaec56d92396b31
islam abdallah <engislamabdoo1995@gmail.com>
2025-05-05 02:30:05 +0300

    remove height

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,7 +33,7 @@ const Footer: React.FC<FooterProps> = ({
   quality = {} 
 }) => {
   return (
-    <footer className="bg-gray-900 text-white pt-16 pb-8 mt-auto h-[200px]">
+    <footer className="bg-gray-900 text-white pt-16 pb-8 mt-auto">
       <div className="container mx-auto px-4 md:px-6">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* Company Info */}
